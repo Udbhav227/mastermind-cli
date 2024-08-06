8ea60a1e4b95cb2c73eceae30514fc3cffc05890
Udbhav Ashish <udbhav227@gmail.com>
2024-08-06 11:31:51 +0530

    add text messages that wil be used in game

---
+++ lib/display.rb
@@ -0,0 +1,30 @@
+require_relative 'instructions'
+
+# class for all text related calls
+module Display
+  include Instructions
+  def show_instruction
+    puts instruction
+  end
+
+  def set_game
+    'The computer has set the ‘master code’ and now it’s time for you to break the code.'
+  end
+
+  def show_turn(turn)
+    puts "Turn ##{turn}: Type in four numbers (1-6) to guess code, or q to quit game."
+  end
+
+  def show_win_message
+    puts 'You broke the code! Congratulations, you win!'
+  end
+
+  def reveal_code(master_code)
+    puts "Here is the 'master code' that you were trying to break"
+    puts master_code
+  end
+
+  def last_turn_warning
+    'Choose carefully. This is your last chance to win!'
+  end
+end
---
+++ lib/instructions.rb
@@ -0,0 +1,12 @@
+# rubocop:disable Metrics/AbcSize
+
+# Instructions on how to play the game
+module Instructions
+  def instruction
+    'It’s time to play!
+    Press ‘1’ to be the code MAKER
+    Press ‘2’ to be the code Breaker'
+  end
+end
+
+# rubocop:enable Metrics/AbcSize
