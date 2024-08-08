aa5981a5f0f9ffcb7276bb25d0b3808081ab92e2
Udbhav Ashish <udbhav227@gmail.com>
2024-08-09 00:04:19 +0530

    minor code refractor

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -2,8 +2,9 @@
 class CodeBreaker
   attr_accessor :master_code
 
-  include Display
   LAST_TURN = 10
+  include Display
+  include GameLogic
   def play
     @master_code = (4.times.map { rand(1..6).to_s }).join
     puts set_game
@@ -17,19 +18,16 @@ class CodeBreaker
       show_turn(turn)
       puts last_turn_warning if turn == LAST_TURN
       guess = gets.chomp
-      break if game_over?(turn, guess)
+      break if game_over?(@master_code, guess, turn, LAST_TURN)
 
-      show_guess_with_hint(guess)
+      show_guess_with_hint(guess, @master_code)
       turn += 1
     end
     show_end_message(turn, guess)
   end
 
-  def game_over?(turn, guess)
-    turn == LAST_TURN || guess == @master_code || %w[q Q].include?(guess)
-  end
-
   def show_end_message(turn, guess)
+    puts "\n"
     if turn == LAST_TURN || %w[q Q].include?(guess)
       show_code(guess) if turn == LAST_TURN
       puts game_over_message
@@ -38,27 +36,5 @@ class CodeBreaker
       show_code(guess)
       show_win_message
     end
-  end
-
-  def generate_hints(guessed_code, master_code = @master_code) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
-    exact_matches = 0
-    guessed_code_counts = Hash.new(0)
-    master_code_counts = Hash.new(0)
-
-    guessed_code.chars.each_with_index do |char, i|
-      if char == master_code[i]
-        exact_matches += 1
-      else
-        guessed_code_counts[char] += 1
-        master_code_counts[master_code[i]] += 1
-      end
-    end
-
-    partial_matches = 0
-    guessed_code_counts.each do |char, count|
-      partial_matches += [count, master_code_counts[char]].min if master_code_counts.key?(char)
-    end
-
-    (['*'] * exact_matches) + (['?'] * partial_matches)
   end
 end
