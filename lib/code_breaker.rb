31f89443c0f73de4f18638dfe43c26aa0b3776d5
Udbhav Ashish <udbhav227@gmail.com>
2024-08-08 05:46:05 +0530

    bug fix : better hint generation

--- lib/code_breaker.rb
+++ lib/code_breaker.rb
@@ -40,29 +40,25 @@ class CodeBreaker
     end
   end
 
-  def generate_hints(guess, master = @master_code)
-    exact_matches = find_exact_matches(master, guess)
-    partial_matches = find_partial_matches(master, guess)
+  def generate_hints(guessed_code, master_code = @master_code) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    exact_matches = 0
+    guessed_code_counts = Hash.new(0)
+    master_code_counts = Hash.new(0)
+
+    guessed_code.chars.each_with_index do |char, i|
+      if char == master_code[i]
+        exact_matches += 1
+      else
+        guessed_code_counts[char] += 1
+        master_code_counts[master_code[i]] += 1
+      end
+    end
+
+    partial_matches = 0
+    guessed_code_counts.each do |char, count|
+      partial_matches += [count, master_code_counts[char]].min if master_code_counts.key?(char)
+    end
 
     (['*'] * exact_matches) + (['?'] * partial_matches)
-  end
-
-  def find_exact_matches(master, guess)
-    master.chars.zip(guess.chars).count { |m, g| m == g }
-  end
-
-  def find_partial_matches(master, guess)
-    master_counts = Hash.new(0)
-    guess_counts = Hash.new(0)
-    
-    master.chars.each { |char| master_counts[char] += 1 }
-    guess.chars.each { |char| guess_counts[char] += 1 }
-  
-    partial_matches = 0
-    master_counts.each do |char, count|
-      partial_matches += [count, guess_counts[char]].min if char != guess_counts[char]
-    end
-  
-    partial_matches
   end
 end
